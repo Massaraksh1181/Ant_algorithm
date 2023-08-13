465a9481c6bc889a8fee42c2194a30ca955a187d
Bogdan <massaraksh1181@yandex.ru>
2023-08-13 17:35:44 +0300

    Version of the algorithm with the dictionary 'key from-to'

--- Ant_Algorithm/Algorithm.cs
+++ Ant_Algorithm/Algorithm.cs
@@ -18,9 +18,10 @@ internal class Algorithm
     Dictionary<char, float> desire = new Dictionary<char, float>(); // словарь с желанием перехода в соседнюю точку
     Dictionary<char, float> probability = new Dictionary<char, float>(); // словарь с вероятностью перехода в соседнюю точку
     List<char> visitedNodes = new List<char>(); // список посещенных узлов одного муравья в этой итерации
+    float distance = 0;
 
     //желание перехода из i в j-ю вершину
-    public List<char> desireСhoice (char nodeAnt) // !!!!!!!!!!!!!!! переменная в параметре - временная мера, та, с которой начинает путь каждый новый муравей
+    public KeyValuePair<List<char>, float> desireСhoice(char nodeAnt) 
     {
         visitedNodes.Add(nodeAnt);
 
@@ -28,48 +29,66 @@ internal class Algorithm
         desire.Clear();
         probability.Clear();
 
-        char nextNode = '-';// следующий выбранный с наибольшей веротяностью узел
+        char nextNode = '+';// следующий выбранный с наибольшей веротяностью узел
 
-            foreach (var node in graph.mapDistancesPheromone) // поиск соседей для выбранной вершины
+        foreach (var node in graph.mapDistancesPheromone) // поиск соседей для выбранной вершины
+        {
+            if ((node.Key.Key == nodeAnt) && (visitedNodes.Contains(node.Key.Value) == false))//если ключ-откуда данного узла совпадает с обрабатываемым сейчас узлом И не был посещен
+                neighbors.Add(node.Key.Value, new KeyValuePair<float, float>(node.Value.Key, node.Value.Value));
+        }
+
+        foreach (var node in neighbors)// заполняем словарь с желанием перехода в каждую соседнюю точку
+        {
+            desire.Add(node.Key, 1 / node.Value.Key * node.Value.Value);// в ключе точка-сосед, в значении формула для расчета желания
+        }
+
+        float desireSum = 0;
+        foreach (var nodeDesire in desire)// высчитываем сумму всех желаний для соседних точек
+        {
+            desireSum += nodeDesire.Value;
+        }
+
+        foreach (var nodeDesire in desire)// высчитываем веротяность перехода для соседних точек
+        {
+            probability.Add(nodeDesire.Key, nodeDesire.Value / desireSum);
+        }
+
+        float temp = 0;
+        foreach (var nodeProbaboloty in probability) // поиск вершины с наибольшей вероятностью
+        {
+            if (nodeProbaboloty.Value > temp)// если вероятность перехода в точку больше чем у предыдущей
             {
-                if (node.Key.Key == nodeAnt)//если ключ-откуда данного узла совпадает с обрабатываемым сейчас узлом
-                    neighbors.Add(node.Key.Value, new KeyValuePair<float, float>(node.Value.Key, node.Value.Value));
+                temp = nodeProbaboloty.Value;
+                nextNode = nodeProbaboloty.Key;
             }
-            foreach (var node in neighbors)// заполняем словарь с желанием перехода в каждую соседнюю точку
-            {
-                desire.Add(node.Key, 1 / node.Value.Key * node.Value.Value);// в ключе точка-сосед, в значении формула для расчета желания
-            }
+        }
 
-            float desireSum = 0;
-            foreach (var nodeDesire in desire)// высчитываем сумму всех желаний для соседних точек
-            {
-                desireSum +=nodeDesire.Value;
-            }
+        KeyValuePair<char, char> distanceNode = new KeyValuePair<char, char>(nodeAnt, nextNode);
 
-            foreach (var nodeDesire in desire)// высчитываем веротяность перехода для соседних точек
-            {
-                probability.Add(nodeDesire.Key, nodeDesire.Value/desireSum);
-            }
+        if (neighbors.Count != 0)//если еще есть соседи для перехода  //if (nextNode.Equals('+')==false) 
+        {
+            distance += graph.mapDistancesPheromone[distanceNode].Key; // подсчет дистанции всего маршрута
+            desireСhoice(nextNode);// рекурсивный вызов  
+        }
+        else // возвращение маршрута в начальную точку
+        { 
+            KeyValuePair<char, char> toStart = new KeyValuePair<char, char>(nodeAnt, visitedNodes[0]);
+            visitedNodes.Add(visitedNodes[0]);
+            distance += graph.mapDistancesPheromone[toStart].Key; // подсчет дистанции всего маршрута с последнем пунктом          
+        }
 
+        KeyValuePair<List<char>, float> oneAntWay = new KeyValuePair<List<char>, float>(visitedNodes, distance);
 
+        return oneAntWay;
+    }
 
-            float temp = 0;
-            foreach (var nodeProbaboloty in probability) // поиск вершины с наибольшей вероятностью
-            {
-                if (nodeProbaboloty.Value > temp)// если вероятность перехода в точку больше чем у предыдущей
-                {
-                    if (visitedNodes.Contains(nodeProbaboloty.Key) == false) // проверка на то, что вершины нет в списке пройденных вершин
-                    {
-                        temp = nodeProbaboloty.Value;
-                        nextNode = nodeProbaboloty.Key;
-                    }
-                }
-            }
-
-        if (nextNode.Equals('-')==false) //если еще есть узел для перехода
-        desireСhoice(nextNode);// рекурсивный вызов
-
-        return visitedNodes;
+    public void clearer()
+    {
+        neighbors.Clear();
+        desire.Clear();
+        probability.Clear();
+        visitedNodes.Clear();
+        distance = 0;
     }
  
     //распределение ферамона по граням 
--- Ant_Algorithm/Program.cs
+++ Ant_Algorithm/Program.cs
@@ -10,22 +10,40 @@ class Program
 
         graph.createGraph('a', 'b', 4, 0.2f);
         graph.createGraph('a', 'c', 3, 0.2f);
-        graph.createGraph('a', 'd', 2, 0.2f);
+        graph.createGraph('a', 'd', 5, 0.2f);
         graph.createGraph('b', 'a', 4, 0.2f);
         graph.createGraph('b', 'c', 7, 0.2f);
         graph.createGraph('b', 'd', 3, 0.2f);
         graph.createGraph('c', 'a', 3, 0.2f);
         graph.createGraph('c', 'b', 7, 0.2f);
         graph.createGraph('c', 'd', 9, 0.2f);
-        graph.createGraph('d', 'a', 4, 0.2f);
+        graph.createGraph('d', 'a', 5, 0.2f);
         graph.createGraph('d', 'b', 3, 0.2f);
         graph.createGraph('d', 'c', 9, 0.2f);
 
         Algorithm algorithm = new Algorithm(ants,graph);
-        algorithm.desireСhoice('a').ForEach(Console.WriteLine);
 
+        KeyValuePair<List<char>, float> oneAntWay = algorithm.desireСhoice('a');
+        oneAntWay.Key.ForEach(Console.WriteLine);
+        Console.WriteLine(oneAntWay.Value);
 
+        algorithm.clearer();
 
+        KeyValuePair<List<char>, float> twoAntWay = algorithm.desireСhoice('b');
+        twoAntWay.Key.ForEach(Console.WriteLine);
+        Console.WriteLine(twoAntWay.Value);
+
+        algorithm.clearer();
+
+        KeyValuePair<List<char>, float> threeAntWay = algorithm.desireСhoice('c');
+        threeAntWay.Key.ForEach(Console.WriteLine);
+        Console.WriteLine(threeAntWay.Value);
+
+        algorithm.clearer();
+
+        KeyValuePair<List<char>, float> fourAntWay = algorithm.desireСhoice('d');
+        fourAntWay.Key.ForEach(Console.WriteLine);
+        Console.WriteLine(fourAntWay.Value);
 
     }
 }
